455173edeb6eaa8cb4ffb0c4fce5d415047f48f2
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2019-05-27 13:15:18 +0800

    add debug code for reactive API test

--- src/main/java/com/example/jeffrey/springcloudtask/SpringCloudTaskApplication.java
+++ src/main/java/com/example/jeffrey/springcloudtask/SpringCloudTaskApplication.java
@@ -3,6 +3,7 @@ package com.example.jeffrey.springcloudtask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,11 @@ public class SpringCloudTaskApplication {
 	private static Logger LOGGER = LoggerFactory.getLogger(SpringCloudTaskApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudTaskApplication.class, args);
+		// SpringApplication.run(SpringCloudTaskApplication.class, args);
+		// Prevent embedded netty server from starting with spring-boot-starter-webflux
+		SpringApplication app = new SpringApplication(SpringCloudTaskApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 
 	@Bean
--- src/main/java/com/example/jeffrey/springcloudtask/configuration/TaskConfig.java
+++ src/main/java/com/example/jeffrey/springcloudtask/configuration/TaskConfig.java
@@ -1,16 +1,12 @@
 package com.example.jeffrey.springcloudtask.configuration;
 
 import com.example.jeffrey.springcloudtask.SpringCloudTaskApplication;
-import com.example.jeffrey.springcloudtask.computation.LengthyWork;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -19,14 +15,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Configuration
 @EnableBatchProcessing
@@ -51,48 +44,35 @@ public class TaskConfig {
 
     @Bean
     public Job job() {
-//        return jobBuilderFactory.get(getJobName())
+        return jobBuilderFactory.get(getJobName())
                 // Need an unique incrementer because jobs use a database to maintain execution state
                 // Spring Batch has the rule that a JobInstance can only be run once to completion.
                 // This means that for each combination of identifying job parameters, only have one
                 // JobExecution that can results in COMPLETE.
 //                .incrementer(new RunIdIncrementer())
-////                .incrementer(new SimpleIncrementer())
-//                .start(stepBuilderFactory.get("job1step1")
-//                        .tasklet((contribution, chunkContext) -> {
-//                            LOGGER.info("Job1 was run");
-//
-//                            // TODO: replace the lengthy computation work
-////                            LengthyWork.testSherlockAndAnagrams();
-//
-//                            // Update the result to remote cache via restful call
-////                            URI uri = URI.create(cacheServiceConfig.writerEndpoint);
-////                            Map<String, String> entity = new HashMap<>();
-////                            entity.put("endpoint", "test4-key");
-////                            entity.put("responseBody", new Date().toString());
-////                            ResponseEntity<?> responseEntity = restTemplate.postForEntity(uri, entity, Map.class);
-////                            LOGGER.info("response: {}", responseEntity.getBody().toString());
-//
-////                            URI uri = URI.create("http://localhost:8081/customers");
-////                            List<Object> objectList = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<List<Object>>() {}).getBody();
-//
-//                            return RepeatStatus.FINISHED;
-//                        })
-//                        .build())
-//                .build();
+                .incrementer(new SimpleIncrementer())
+                .start(stepBuilderFactory.get("job1step1")
+                        .tasklet((contribution, chunkContext) -> {
+                            LOGGER.info("Job1 was run");
 
-        return this.jobBuilderFactory.get("job1")
-                .start(this.stepBuilderFactory.get("job1step1")
-                        .tasklet(new Tasklet() {
-                            @Override
-                            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                                LOGGER.info("Job1 was run");
-                                return RepeatStatus.FINISHED;
-                            }
+                            // TODO: replace the lengthy computation work
+//                            LengthyWork.testSherlockAndAnagrams();
+
+                            // Update the result to remote cache via restful call
+//                            URI uri = URI.create(cacheServiceConfig.writerEndpoint);
+//                            Map<String, String> entity = new HashMap<>();
+//                            entity.put("endpoint", "test4-key");
+//                            entity.put("responseBody", new Date().toString());
+//                            ResponseEntity<?> responseEntity = restTemplate.postForEntity(uri, entity, Map.class);
+//                            LOGGER.info("response: {}", responseEntity.getBody().toString());
+
+                            URI uri = URI.create("http://localhost:8081/customers");
+                            List<Object> objectList = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<List<Object>>() {}).getBody();
+
+                            return RepeatStatus.FINISHED;
                         })
                         .build())
                 .build();
-
     }
 
     protected String getJobName() {
